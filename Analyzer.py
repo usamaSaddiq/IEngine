ef521bc34c09b521bacfa9bb55b967312208a3f8
Usama Saddiq <20945081+usamaSaddiq@users.noreply.github.com>
2018-05-23 13:08:23 +1000

    Initial analyzer parsing single charahcter strings

---
+++ Analyzer.py
@@ -0,0 +1,76 @@
+from Tokenizer import Tokenizer
+from Symbols import *
+
+class Analyzer(Tokenizer):
+    """
+    Analyser for propositional logic
+    """
+    def __init__(self, input):
+        super().__init__(input)
+        self.data = {"~" : NSymbol, "&" : ASymbol, "\\/" : OSymbol, "TRUE" : TSymbol, "FALSE" : FSymbol}
+
+    def get_next_char_to_analyse(self):
+        # jump the spaces and get to the next charachter
+        self.jump_spaces()
+        char = self.next_char()
+        # check for or operator
+        if char == '~':
+            return NSymbol()
+        if char == '\\':
+            return self.or_char()
+        # check for and operator            
+        if char == '&':
+            return ASymbol()
+        # check for opening paranthesis operator
+        if char == '(':
+            return LSymbol()
+        # check for closing paranthesis
+        if char == ')':
+            return RSymbol()
+        # check for bicondition
+        if char == '<':
+            return self.bicond_char()
+        # check for implication
+        if char == '=':
+            return self.impl_char()
+        # check for end of line
+        if char == None:
+            return ESymbol()
+        # charchter is a symbol
+        if char.isalpha():
+            self.decrement_position()
+            return self.get_identifier()
+        
+        raise Exception("Unexpected symbol " + char)
+
+    def or_char(self):
+        self.predict('/')
+        return OSymbol()
+
+    def bicond_char(self):
+        self.predict('=')
+        self.predict('>')
+        return BSymbol()
+
+    def impl_char(self):
+        self.predict('>')
+        return ImSymbol()
+
+    def get_identifier(self):
+        content = self.next_char()
+        while True:
+            char = self.next_char()
+            if char != None and char.isalnum():
+                content += char
+            else:
+                if char != None:
+                    self.decrement_position()
+                break
+        return self.create_identifier(content)
+
+    def create_identifier(self, content):
+        unknown_identifier = self.data.get(content)
+        if not unknown_identifier:
+            return IDSymbol(content)  
+        return unknown_identifier()
+
